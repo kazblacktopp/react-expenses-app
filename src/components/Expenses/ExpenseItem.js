1242561ab82aa4e1c62ce1a61007d08fe6675df4
kazblacktopp <karen.blacktopp@gmail.com>
2022-06-21 21:12:45 +1000

    Fixed expense amounts to two decimal places before rendering

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,15 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const amount = props.amount.toFixed(2);
+
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
-          <p className="expense-item__price">${props.amount}</p>
+          <p className="expense-item__price">${amount}</p>
         </div>
       </Card>
     </li>
