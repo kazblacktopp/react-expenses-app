24091bdcf1dd495c5a50c1f772ea7627cb397610
kazblacktopp <karen.blacktopp@gmail.com>
2022-06-21 21:14:30 +1000

    Added state for filteredYear and passed to ExpensesList component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 function Expenses(props) {
-  const expenses = props.items;
+  const [filteredYear, setFilteredYear] = useState('all');
+  const filteredExpenses =
+    filteredYear === 'all'
+      ? props.items
+      : props.items.filter(
+          item => item.date.getFullYear().toString() === filteredYear
+        );
+
+  function expenseFilterHandler(selectedYear) {
+    setFilteredYear(selectedYear);
+  }
 
   return (
     <Card className="expenses">
-      <ExpensesList items={expenses} />
+      <ExpensesFilter onExpenseFilter={expenseFilterHandler} />
+      <ExpensesList items={filteredExpenses} filteredYear={filteredYear} />
     </Card>
   );
 }
