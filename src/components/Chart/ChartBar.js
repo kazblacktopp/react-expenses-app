e9b91271081764f73039d1e95d7e670851694fd1
kazblacktopp <karen.blacktopp@gmail.com>
2022-06-22 19:30:24 +1000

    Used condition to calculate percentage for height of chart bars

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,7 +1,9 @@
 import './ChartBar.css';
 
 function ChartBar(props) {
-  const barFillPercentage = (props.value / props.maxValue) * 100;
+  const barFillPercentage = `${
+    props.value > 0 ? Math.round((props.value / props.maxValue) * 100) : 0
+  }%`;
 
   return (
     <div className="chart-bar">
